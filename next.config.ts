af2a5c1c619d594e0fcf5619777b749ba60bd774
Deepjyoti13 <deepjyoti.de@logward.com>
2026-05-13 11:28:03 +0530

    Fixing logo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    localPatterns: [{ pathname: '/uploads/**' }],
+    localPatterns: [{ pathname: '/**' }],
     remotePatterns: [
       {
         protocol: 'https',
